d1535892e94c862e2184ccfcaea5b01d1456c608
matt-heindel <mheindel94@gmail.com>
2021-06-15 20:34:33 -0700

    Include gitconfig, add git alias 'gmh'

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc bash_profile bash_aliases"    # list of files/folders to symlink in homedir
+files="bashrc vimrc bash_profile bash_aliases gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
