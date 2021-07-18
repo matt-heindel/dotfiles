6de6a2598f63abe623e0312c5f8acdc4e5e147a7
Matt Heindel <mheindel94@gmail.com>
2021-07-18 11:12:13 -0700

    Remove gitconfig from tracked files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,7 +9,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 # update files to match dir
-files="bashrc vimrc bash_profile bash_aliases gitconfig"    # list of files/folders to symlink in homedir
+files="bashrc vimrc bash_profile bash_aliases"    # list of files/folders to symlink in homedir
 
 ##########
 
